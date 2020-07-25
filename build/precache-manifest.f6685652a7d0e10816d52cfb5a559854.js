fc0cd0c758817b9a3f9af7c77117e7d864cda20c
Alaa <alaa00nasser@gmail.com>
2020-07-25 18:37:17 +0100

    new_commit

---
+++ build/precache-manifest.f6685652a7d0e10816d52cfb5a559854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb99b040c711475ef3e3deb47775ffa",
+    "url": "/react-covid-19/index.html"
+  },
+  {
+    "revision": "dc90204b7c6c6561ebe1",
+    "url": "/react-covid-19/static/css/main.186980c8.chunk.css"
+  },
+  {
+    "revision": "9731354b0b05a5d217fa",
+    "url": "/react-covid-19/static/js/2.3e540c99.chunk.js"
+  },
+  {
+    "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
+    "url": "/react-covid-19/static/js/2.3e540c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc90204b7c6c6561ebe1",
+    "url": "/react-covid-19/static/js/main.ba72bc06.chunk.js"
+  },
+  {
+    "revision": "c1122267bb4f31d99193",
+    "url": "/react-covid-19/static/js/runtime-main.1435bc3d.js"
+  }
+]);
